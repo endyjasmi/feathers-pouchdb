fffd1e61a65fdac23561960f604424f28db9da00
Endy Jasmi <endyjasmi@gmail.com>
2017-07-01 12:18:11 +0800

    Cleaned service method, to be re-implemented.

--- src/index.js
+++ src/index.js
@@ -2,9 +2,7 @@ import errors from 'feathers-errors';
 import makeDebug from 'debug';
 import { select } from 'feathers-commons';
 
-import { convertQuery,
-  createMultiple,
-  createSingle } from './utils';
+import { create, find, update, path, remove } from './utils';
 
 const debug = makeDebug('feathers-pouchdb');
 
@@ -18,40 +16,12 @@ class Service {
   }
 
   create(data, params) {
-    const createPromise = Array.isArray(data) ? createMultiple(data) : createSingle(data);
-    return createPromise.then(select(params, '_id'));
+    data = Array.isArray(data) ? data : [data];
+    return create(this.Model, data).then(select(params, '_id'));
   }
 
   find(params) {
-    params = params || {};
-    params.query = params.query || { _id: { $gte: null }};
-
-    if (params.query.$select &&
-      Array.isArray(params.query.$select) &&
-      params.query.$select.indexOf('_id') === -1) {
-      params.query.$select.push('_id');
-    }
-
-    return this.Model.find(convertQuery(params.query))
-      .then(response => {
-        return response.docs;
-      })
-      .then(select(params, '_id'));
-  }
-
-  get(id, params) {
-    params = params || {};
-    params.query = params.query || {};
-    params.query['_id'] = id;
-    params.query.$limit = 1;
-
-    return this.find(params)
-      .then(result => {
-        if (result.length < 1) {
-          throw new errors.NotFound(`No record found for _id '${id}'`);
-        }
-        return result[0];
-      });
+    return find(this.Model, params).then(select(params, '_id'));
   }
 
 }
